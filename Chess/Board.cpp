6d0133385002c391b16b6f610ebc19c6515b5741
MingYanWoo <woomingyan@gmail.com>
2018-05-08 01:46:51 +0800

    Chess board finish

--- Chess/Board.cpp
+++ Chess/Board.cpp
@@ -12,7 +12,22 @@ void Board::paintEvent(QPaintEvent *)
     int d = 40;
     for(int i = 1; i <= 10; ++i)
     {
-        painter.setPen(QColor(255, 1, 0));
         painter.drawLine(QPoint(d, i*d), QPoint(9*d, i*d));
     }
+    for(int i = 1; i <= 9; ++i)
+    {
+        if(i == 1 || i == 9)
+        {
+            painter.drawLine(QPoint(i*d, d), QPoint(i*d, 10*d));
+        }
+        else
+        {
+            painter.drawLine(QPoint(i*d, d), QPoint(i*d, 5*d));
+            painter.drawLine(QPoint(i*d, 6*d), QPoint(i*d, 10*d));
+        }
+    }
+    painter.drawLine(QPoint(4*d, d), QPoint(6*d, 3*d));
+    painter.drawLine(QPoint(6*d, d), QPoint(4*d, 3*d));
+    painter.drawLine(QPoint(4*d, 8*d), QPoint(6*d, 10*d));
+    painter.drawLine(QPoint(6*d, 8*d), QPoint(4*d, 10*d));
 }
